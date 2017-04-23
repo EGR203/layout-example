07c76cb6d82c34f2e91cbce4c28c8835f89a411f
EGR203 <patrick128500@gmail.com>
2017-04-23 14:37:59 +1000

    Delted post-scroll (bad work on chrome, for correct work need JQuery)

--- js/script.js
+++ js/script.js
@@ -8,58 +8,5 @@ window.addEventListener('load', function(){
 	});
 	//-------------------------------------------------
 	//-------------------------------------------------
-	
-	
-	//Возвращает Y координату относително всей страницы
-	function getOffsetWindow(elem) {
-		var top=0;
-		while(elem) {
-			top = top + parseInt(elem.offsetTop);
-			elem = elem.offsetParent;
-		}
-		return top;
-	}
 
-	//Возвращает Y координату относително всей дисплея пользователя
-	function getOffsetDisplay(elem){
-		return getOffsetWindow(elem) - window.scrollY;
-	}
-	//key points - элементы интерфейса, на которых срабатывает post-scroll
-	var keyPoints = document.getElementsByClassName('key-point');
-	var postScrolling = (function(){
-		//переменные для проверки на мультискролл
-		var triggerNum = 0;
-		var lastTriggerElem = null;
-		var MAX_TRIGGER_NUM = 2;
-		
-		return function (){
-			//расстояние (в пикселях) срабатывания post-scroll`а
-			let SCROLL_DISTANCE = 50;
-			for(let i = 0; i<keyPoints.length; i++){
-				let elem = keyPoints[i];
-				let elemOffset = getOffsetDisplay(elem);
-				//Если скролл отдалился от последнего элемента на большое расстояние, скидываем счетчик срабатывания
-				if(lastTriggerElem !== null && getOffsetDisplay(lastTriggerElem) > SCROLL_DISTANCE * 3){
-					triggerNum = 0;
-				}
-				//проверка на большое количество post-scroll`ов на 1 элементе
-				let multiScroll = ! (triggerNum < MAX_TRIGGER_NUM || lastTriggerElem != elem) ;
-				//Проверка условий на мультискролл и положения элемента
-				if( ! multiScroll && elemOffset <= SCROLL_DISTANCE && elemOffset > 0 ) {
-					window.scroll(0, getOffsetWindow(elem));
-
-					if(lastTriggerElem == elem){
-						triggerNum++;
-					}else{
-						triggerNum = 0;
-						lastTriggerElem = elem;
-					}
-				}
-			}
-		};
-	})();
-
-	window.addEventListener('scroll', postScrolling);
-	//-------------------------------------------------
-	//-------------------------------------------------
 });
